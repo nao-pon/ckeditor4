ae34a16901c2320576052d62cdd5892c0c4396c0
nao-pon <hypweb@gmail.com>
2012-09-16 23:54:51 +0900

    fix typo

--- html/modules/ckeditor4/admin/index.php
+++ html/modules/ckeditor4/admin/index.php
@@ -1,7 +1,7 @@
 <?php
 
-require_once require_once "../../../mainfile.php";
-require_once XOOPS_ROOT_PATH . "/header.php";
+require_once '../../../mainfile.php';
+require_once XOOPS_ROOT_PATH . '/header.php';
 
 //redirect_header(XOOPS_MODULE_URL . "/legacy/admin/index.php?action=Help&amp;dirname=ckeditor4", 1, "Loading...!");
 $root =& XCube_Root::getSingleton();
