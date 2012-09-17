8c277446009fb9786c3ba2768694b24d8d09a737
nao-pon <hypweb@gmail.com>
2012-09-17 23:39:44 +0900

    fix BBCode rendering [img] & [siteimg]

--- html/modules/ckeditor4/preload/TextFilter.class.php
+++ html/modules/ckeditor4/preload/TextFilter.class.php
@@ -19,25 +19,15 @@ class ckeditor4_TextFilter extends XCube_ActionFilter
 	
 	function filter(&$patterns, &$replacements)
 	{
-		// [img align=left width=10 height=10]
-		$patterns[] = '/\[img(?:\s+align=([\'"]?)(left|center|right)\1)?(?:\s+w(?:idth)?=([\'"]?)([\d]+?)\3)?(?:\s+h(?:eight)?=([\'"]?)([\d]+?)\5)?]([!~*\'();\/?:\@&=+\$,%#\w.-]+)\[\/img\]/US';
-		$replacements[0][] = '<a href="$7" target="_blank">$7</a>';
-		$replacements[1][] = '<img src="$7" align="$2" width="$4" height="$6" alt="" />';
-		
 		// [img align=left title=hoge width=10 height=10]
-		$patterns[] = '/\[img(?:\s+align=([\'"]?)(left|center|right)\1)?\s+title=([\'"]?)((?(3).*|[^\]\s]*))\3(?:\s+w(?:idth)?=([\'"]?)([\d]+?)\5)?(?:\s+h(?:eight)?=([\'"]?)([\d]+?)\7)?]([!~*\'();\/?:\@&=+\$,%#\w.-]+)\[\/img\]/USe';
-		$replacements[0][] = '\'<a href="$9" target="_blank">$9</a>\'';
-		$replacements[1][] = '\'<img src="$9" align="$2" width="$6" height="$8" alt="\'.htmlspecialchars(str_replace(\'\\"\', \'"\', \'$4\')).\'" title="\'.htmlspecialchars(str_replace(\'\\"\', \'"\', \'$4\')).\'" />\'';
+		$patterns[] = '/\[img(?:\s+align=(&quot;|&#039;)?(left|center|right)(?(1)\1))?(?:\s+title=(&quot;|&#039;)?((?(3)[^]]*|[^\]\s]*))(?(3)\3))?(?:\s+w(?:idth)?=(&quot;|&#039;)?([\d]+?)(?(5)\5))?(?:\s+h(?:eight)?=(&quot;|&#039;)?([\d]+?)(?(7)\7))?]([!~*\'();\/?:\@&=+\$,%#\w.-]+)\[\/img\]/US';
+		$replacements[0][] = '<a href="$9" title="$4" target="_blank">$9</a>';
+		$replacements[1][] = '<img src="$9" align="$2" width="$6" height="$8" alt="$4" title="$4" />';
 
-		// [siteimg align=left width=10 height=10]
-		$patterns[] = '/\[siteimg(?:\s+align=([\'"]?)(left|center|right)\1)?(?:\s+w(?:idth)?=([\'"]?)([\d]+?)\3)?(?:\s+h(?:eight)?=([\'"]?)([\d]+?)\5)?]([!~*\'();\/?:\@&=+\$,%#\w.-]+)\[\/siteimg\]/US';
-		$replacements[0][] = 
-		$replacements[1][] = '<img src="'.XOOPS_URL.'/$7" align="$2" width="$4" height="$6" alt="" />';
-		
 		// [siteimg align=left title=hoge width=10 height=10]
-		$patterns[] = '/\[siteimg(?:\s+align=([\'"]?)(left|center|right)\1)?\s+title=([\'"]?)((?(3).*|[^\]\s]*))\3(?:\s+w(?:idth)?=([\'"]?)([\d]+?)\5)?(?:\s+h(?:eight)?=([\'"]?)([\d]+?)\7)?]([!~*\'();\/?:\@&=+\$,%#\w.-]+)\[\/siteimg\]/USe';
+		$patterns[] = '/\[siteimg(?:\s+align=(&quot;|&#039;)?(left|center|right)(?(1)\1))?(?:\s+title=(&quot;|&#039;)?((?(3)[^]]*|[^\]\s]*))(?(3)\3))?(?:\s+w(?:idth)?=(&quot;|&#039;)?([\d]+?)(?(5)\5))?(?:\s+h(?:eight)?=(&quot;|&#039;)?([\d]+?)(?(7)\7))?]([!~*\'();\/?:\@&=+\$,%#\w.-]+)\[\/siteimg\]/US';
 		$replacements[0][] = 
-		$replacements[1][] = '\'<img src="'.XOOPS_URL.'/$9" align="$2" width="$6" height="$8" alt="\'.htmlspecialchars(str_replace(\'\\"\', \'"\', \'$4\')).\'" title="\'.htmlspecialchars(str_replace(\'\\"\', \'"\', \'$4\')).\'" />\'';
+		$replacements[1][] = '<img src="'.XOOPS_URL.'/$9" align="$2" width="$6" height="$8" alt="$4" title="$4" />';
 	}
 }
 
