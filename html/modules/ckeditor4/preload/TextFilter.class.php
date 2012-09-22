17d229d0c1a854b9159c78c13823842be69eaaa4
nao-pon <hypweb@gmail.com>
2012-09-22 10:31:01 +0900

    code clean up

--- html/modules/ckeditor4/preload/TextFilter.class.php
+++ html/modules/ckeditor4/preload/TextFilter.class.php
@@ -28,8 +28,9 @@ class ckeditor4_TextFilter extends XCube_ActionFilter
 		$patterns[] = '/\[siteimg(?:\s+align=(&quot;|&#039;)?(left|center|right)(?(1)\1))?(?:\s+title=(&quot;|&#039;)?((?(3)[^]]*|[^\]\s]*))(?(3)\3))?(?:\s+w(?:idth)?=(&quot;|&#039;)?([\d]+?)(?(5)\5))?(?:\s+h(?:eight)?=(&quot;|&#039;)?([\d]+?)(?(7)\7))?]([!~*\'();\/?:\@&=+\$,%#\w.-]+)\[\/siteimg\]/US';
 		$replacements[0][] = 
 		$replacements[1][] = '<img src="'.XOOPS_URL.'/$9" align="$2" width="$6" height="$8" alt="$4" title="$4" />';
-	
+
 		// [list] nested allow
+		/// list style
 		$list_open = '("$1"=="1"?"ol":
 		("$1"=="a"?"ol style=\"list-style-type:lower-alpha\"":
 		("$1"=="A"?"ol style=\"list-style-type:upper-alpha\"":
@@ -40,38 +41,20 @@ class ckeditor4_TextFilter extends XCube_ActionFilter
 		("$1"=="C"?"ul style=\"list-style-type:circle\"":
 		("$1"=="S"?"ul style=\"list-style-type:square\"":"ul")))))))))';
 		$list_close = '(("$1"=="1"||"$1"=="a"||"$1"=="A"||"$1"=="r"||"$1"=="R"||"$1"=="d")?"ol":"ul")';
-		
-// 		$patterns[] = '/\[list(?:\=([^\]]+))?\](?:\r\n|[\r\n])(.+(?!\[\list[^\]]*?\])|(?R))\[\/list\](?:\r\n|[\r\n])/esS';
-// 		$replacements[0][] = '\'<\'.'.$list_open.'.\'>\'.preg_replace(array(\'/\[list[^\]]*\]/\',\'/\[\/list\]/\'),array("\x01","\x02"),\'$2\').\'</\'.'.$list_close.'.\'>\'';
-
-// 		$patterns[] = '/\[\*\](.*?)(?:\r\n|[\r\n])([\r\n]*)(?=(?:\\[\*\]|<\/[uo]l>))/sS';
-// 		$replacements[0][] = '<li>$1$2</li>';
-		
-// 		$patterns[] = '/(?:\r\n|[\r\n])\x01<\/li>/';
-// 		$replacements[0][] = '<ul>';
-
-// 		$patterns[] = '/(?:\r\n|[\r\n])\x02/';
-// 		$replacements[0][] = '</li></ul>';
-
-
 		/// pre convert
 		$patterns[] = '/\[list/';
 		$replacements[0][] = "\x01";
 		$patterns[] = '/\[\/list\]/';
 		$replacements[0][] = "\x02";
-		
 		/// outer matting
 		$patterns[] = '/\x01(?:\=([^\]]+))?\](?:\r\n|[\r\n])((?:(?>[^\x01\x02]+)|(?R))*)\x02(?:\r\n|[\r\n]|$)/eS';
 		$replacements[0][] = '"<".'.$list_open.'.">$2</".'.$list_close.'.">"';
-		
 		/// [*] to <li></li>
 		$patterns[] = '/\[\*\](.*?)(?:\r\n|[\r\n])([\r\n]*)(?=(?:\\[\*\]|<\/[uo]l>|[\x01\x02]))/sS';
 		$replacements[0][] = '<li>$1$2</li>';
-		
 		/// post convert 1
 		$patterns[] = '/<\/li>\x01[^\]]*\](?:\r\n|[\r\n])/';
 		$replacements[0][] = '<ul>';
-		
 		/// post convert 2
 		$patterns[] = '/\x02(?:\r\n|[\r\n])/';
 		$replacements[0][] = '</ul></li>';
