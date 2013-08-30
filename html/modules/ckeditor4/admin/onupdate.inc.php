7f3a7c4e198e20b22fe639b910fe6a486f49d913
nao-pon <hypweb@gmail.com>
2013-08-30 15:34:59 +0900

    fix typo 'head' to '<head>' for version <= 0.38

--- html/modules/ckeditor4/admin/onupdate.inc.php
+++ html/modules/ckeditor4/admin/onupdate.inc.php
@@ -3,14 +3,26 @@ function xoops_module_update_ckeditor4() {
 	$module_handler = xoops_gethandler('module');
 	$Module = $module_handler->getByDirname('ckeditor4');
 	$config_handler = xoops_gethandler('config');
-	$ModuleConfig = $config_handler->getConfigsByCat(0, $Module->mid());
+	$mid = $Module->mid();
+	$ModuleConfig = $config_handler->getConfigsByCat(0, $mid);
 	if (substr($ModuleConfig['toolbar_user'], -4) === '""]]') {
 		//fix typo '""]]' to '"]]' for version <= 0.37
-		$criteria = new CriteriaCompo(new Criteria('conf_modid', $Module->mid()));
+		$criteria = new CriteriaCompo(new Criteria('conf_modid', $mid));
 		$criteria->add(new Criteria('conf_catid', 0));
 		$criteria->add(new Criteria('conf_name', 'toolbar_user'));
 		if ($configs = $config_handler->_cHandler->getObjects($criteria)) {
 			$val = str_replace('""]]', '"]]', $ModuleConfig['toolbar_user']);
+			$configs[0]->setVar('conf_value', $val, true);
+			$config_handler->insertConfig($configs[0]);
+		}
+	}
+	if (preg_match('/^head\s*$/m', $ModuleConfig['contentsCss'])) {
+		//fix typo 'head' to '<head>' for version <= 0.38
+		$criteria = new CriteriaCompo(new Criteria('conf_modid', $mid));
+		$criteria->add(new Criteria('conf_catid', 0));
+		$criteria->add(new Criteria('conf_name', 'contentsCss'));
+		if ($configs = $config_handler->_cHandler->getObjects($criteria)) {
+			$val = preg_replace('/^head(\s*)$/m', '<head>$1', $ModuleConfig['contentsCss']);
 			$configs[0]->setVar('conf_value', $val, true);
 			$config_handler->insertConfig($configs[0]);
 		}
