d64b557add7d3ff75753ffc2948a6eb4cf0db41d
nao-pon <hypweb@gmail.com>
2014-04-02 13:13:27 +0900

    ckeditor4 version 0.51

--- html/modules/ckeditor4/xoops_version.php
+++ html/modules/ckeditor4/xoops_version.php
@@ -11,7 +11,7 @@ if (!defined('XOOPS_ROOT_PATH')) exit();
 // Define a basic manifesto.
 //
 $modversion['name'] = _MI_CKEDITOR4_LANG_CKEDITOR4;
-$modversion['version'] = 0.50;
+$modversion['version'] = 0.51;
 $modversion['description'] = _MI_CKEDITOR4_DESC_CKEDITOR4 . ' (CKEditor 4.3.4 Full version)';
 $modversion['author'] = "nao-pon http://xoops.hypweb.net/";
 $modversion['credits'] = "Naoki Sawada aka nao-pon";
