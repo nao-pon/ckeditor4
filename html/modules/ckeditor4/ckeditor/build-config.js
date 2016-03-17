3dad29f90d8248cf74cca960aea455adbe399387
nao-pon <hypweb@gmail.com>
2016-03-17 22:03:38 +0900

    CKEditor up to 4.5.7

--- html/modules/ckeditor4/ckeditor/build-config.js
+++ html/modules/ckeditor4/ckeditor/build-config.js
@@ -1,5 +1,5 @@
 ﻿/**
- * @license Copyright (c) 2003-2015, CKSource - Frederico Knabben. All rights reserved.
+ * @license Copyright (c) 2003-2016, CKSource - Frederico Knabben. All rights reserved.
  * For licensing, see LICENSE.md or http://ckeditor.com/license
  */
 
@@ -13,10 +13,10 @@
  * (1) http://ckeditor.com/builder
  *     Visit online builder to build CKEditor from scratch.
  *
- * (2) http://ckeditor.com/builder/fd718e19fbde7ff863d34eebf6c7c332
+ * (2) http://ckeditor.com/builder/8aad4480126816779f658adb23d363c0
  *     Visit online builder to build CKEditor, starting with the same setup as before.
  *
- * (3) http://ckeditor.com/builder/download/fd718e19fbde7ff863d34eebf6c7c332
+ * (3) http://ckeditor.com/builder/download/8aad4480126816779f658adb23d363c0
  *     Straight download link to the latest version of CKEditor (Optimized) with the same setup as before.
  *
  * NOTE:
@@ -122,6 +122,7 @@ var CKBUILDER_CONFIG = {
 		'cy' : 1,
 		'da' : 1,
 		'de' : 1,
+		'de-ch' : 1,
 		'el' : 1,
 		'en' : 1,
 		'en-au' : 1,
